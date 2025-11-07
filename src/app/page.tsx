723dd23df08a308890be1058906d63084b45471d
locxuandao <locxuandao260520033@gmail.com>
2025-11-07 18:05:25 +0700

    test husky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>test</h1>
+      <h1>test husky</h1>
     </div>
   );
 }
